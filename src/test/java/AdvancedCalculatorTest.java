d09f1609f40e51018c777365e846e8bf0cced9bd
pakphoom <pakphoom_k@cmu.ac.th>
2025-09-29 14:06:07 +0700

    add AdvancedCalculatorTest.java

---
+++ src/test/java/AdvancedCalculatorTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+import se327.AdvancedCalculator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AdvancedCalculatorTest {
+    @Test
+    void testPower() {
+        AdvancedCalculator calculator = new AdvancedCalculator();
+        assertEquals(8.0, calculator.power(2, 3), 0.01);
+    }
+
+    @Test
+    void testSqrt() {
+        AdvancedCalculator calculator = new AdvancedCalculator();
+        assertEquals(2.0, calculator.sqrt(4), 0.01);
+    }
+}
